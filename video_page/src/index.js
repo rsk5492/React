c2e3aa0e4add31116b51a9e032d3db8367024868
Srinivas Annapantula <ssk5492@gmail.com>
2017-11-10 03:51:12 -0800

    Basic Video player
    Build a basic video player with default hardcoded search parameter and ability to select different videos from the list.

--- video_page/src/index.js
+++ video_page/src/index.js
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSeach from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 
 const API_KEY = 'AIzaSyBciZGWAQ0MpIRavSK-VQkeaLhQka57O2I';
 
 // Create a new component. This component should produce some HTML.
 
-const App = function() {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+         };
+
+        YTSeach({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ 
+                videos : videos,
+                selectedVideo : videos[0]    
+            });
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video = {this.state.selectedVideo} />
+                <VideoList 
+                    videos={this.state.videos}
+                    onVideoSelect = {selectedVideo => this.setState({selectedVideo})} />
+            </div>
+        );
+    }
 }
 
 
